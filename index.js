6c5374fdccf661625600f2dd37844495cc237967
ggustilo <beastifer@gmail.com>
2017-12-10 15:24:49 -0600

    Add css for different colored speech bubbles

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var client = io.of('/client');
 var commandCenter = io.of('/commandCenter');
+var clients = [];
 
 app.get('/client', function(req, res){
   res.sendFile(__dirname + '/client.html');
@@ -12,27 +13,28 @@ app.get('/commandCenter', function(req, res){
     res.sendFile(__dirname + '/command.html');
   });
 
-client.on('connection', function(socket){
-  console.log('a user connected to client');
-  socket.on('disconnect', function(){
-    console.log('user disconnected to client');
-  });
-  socket.on('chat message', function(msg){
-    commandCenter.emit('chat message', msg);
-    console.log(msg);
-  });
+client.on('connection', function(socket) {
+    clients.push(socket.id);
+    console.log('a user connected to client');
+    socket.on('disconnect', function(){
+        console.log('user disconnected to client');
+    });
+    socket.on('chat message', function(msg){
+        commandCenter.emit('chat message', msg);
+        console.log("from client " + socket.id + ": " + msg);
+    });
 });
 
 commandCenter.on('connection', function(socket){
     console.log('a user connected to command');
     socket.on('disconnect', function(){
-      console.log('user disconnected from command');
+        console.log('user disconnected from command');
     });
     socket.on('chat message', function(msg){
-      client.emit('chat message', msg);
-      console.log(msg);
+        client.emit('chat message', msg);
+        console.log("from commandCenter" + socket.id + ": " + msg);
     });
-  });
+});
 
 http.listen(4050, function(){
   console.log('listening on *:4050');
